bcd373874e522734f9ee904fdce02006a8129388
Rektor96 <maugonzalez96@hotmail.com>
2022-02-11 11:26:22 -0300

    practicamos for loop

---
+++ forloop.js
@@ -0,0 +1,10 @@
+let Nombre = "Pablo gordo puto de mierda";
+
+for (let Cantidad = 0; Cantidad < Nombre.length; Cantidad++){
+    
+    let contadorDeLetras = Cantidad + 1;
+    
+    console.log("La cantidad de Letras de "+Nombre+ " es "+ contadorDeLetras +" Wachin")
+}
+
+//for loop que cuente la cantidad de letras en tu nombre.
